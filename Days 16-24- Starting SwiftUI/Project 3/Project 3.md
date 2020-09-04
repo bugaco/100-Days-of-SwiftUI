a36c56990653802b871a500403ac7809d1036f0c
ZanyZephyr <liyizhezhe@gmail.com>
2020-09-04 09:38:22 +0800

    Project 3 inited

---
+++ Days 16-24- Starting SwiftUI/Project 3/Project 3.md
@@ -0,0 +1,3 @@
+# Project 3
+
+## 1.Why does SwiftUI use structs for views?
---
+++ Days 16-24- Starting SwiftUI/Project 3/ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by BugaCo on 2020/9/4.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
